b9e77f610dd3fc25a19fdbb11dce3fc8de0b025c
Poulomi Mitra <pompyavi@gmail.com>
2025-08-27 00:16:31 +0530

    Adding configs and constants

---
+++ constants/app_constants.py
@@ -0,0 +1,57 @@
+LOGIN_PAGE_TITLE = 'Account Login'
+ACCOUNT_PAGE_TITLE = 'My Account'
+
+RIGHT_COLUMN_LINKS_BEFORE_LOGIN = ['Login', 'Register', 'Forgotten Password', 'My Account', 'Address Book', 'Wish List',
+                      'Order History', 'Downloads', 'Recurring payments', 'Reward Points', 'Returns', 'Transactions',
+                      'Newsletter']
+
+RIGHT_COLUMN_LINKS_AFTER_LOGIN = ["My Account", "Edit Account", "Password", "Address Book",
+    "Wish List", "Order History", "Downloads", "Recurring payments", "Reward Points", "Returns", "Transactions", "Newsletter", "Logout"]
+
+LOGIN_PAGE_URL = 'https://demo.opencart.com/index.php?route=account/login'
+ACCOUNT_PAGE_URL = 'https://naveenautomationlabs.com/opencart/index.php?route=account/account'
+
+ACCOUNT_HEADERS = ['My Account', 'My Orders', 'My Affiliate Account', 'Newsletter']
+
+FOOTER_SECTIONS = ["Information", "Customer Service", "Extras", "My Account"]
+
+FOOTER_LINKS = ["About Us", "Delivery Information", "Privacy Policy", "Terms & Conditions",
+    "Contact Us", "Returns", "Site Map", "Brands", "Gift Certificates", "Affiliate", "Specials", "My Account", "Order History", "Wish List", "Newsletter"]
+
+INFORMATION_SECTION_LINKS = ["About Us", "Delivery Information", "Privacy Policy", "Terms & Conditions"]
+
+USER_REGISTER_SUCCESS_MESG = "Your Account Has Been Created!"
+
+PRODUCTS_INFO = {
+    "MacBook Pro": {
+        "Brand": "Apple",
+        "Availability": "Out Of Stock",
+        "productHeader": "MacBook Pro",
+        "price": "$2,000.00",
+        "Ex Tax": "$2,000.00",
+        "Product Code": "Product 18",
+        "Reward Points": "800"
+    },
+    "Samsung Galaxy Tab 10.1": {
+        "Availability": "Pre-Order",
+        "productHeader": "Samsung Galaxy Tab 10.1",
+        "price": "$241.99",
+        "Ex Tax": "$199.99",
+        "Product Code": "SAM1",
+        "Reward Points": "1000"
+    },
+    "MacBook Air": {
+        "Brand": "Apple",
+        "Availability": "Out Of Stock",
+        "productHeader": "MacBook Air",
+        "price": "$1,202.00",
+        "Ex Tax": "$1,000.00",
+        "Product Code": "Product 17",
+        "Reward Points": "800"
+        }
+    # …add more products here
+}
+
+SHORT_WAIT = 5
+MEDIUM_WAIT = 10
+LONG_WAIT = 20
---
+++ constants/test_data.py
@@ -0,0 +1,18 @@
+# Product Testing Data
+PRODUCT_TEST_DATA = {
+    'macbook_products': [
+        ('macbook', 'MacBook Pro'),
+        ('macbook', 'MacBook Air')
+    ],
+    'samsung_products': [
+        ('samsung', 'Samsung Galaxy Tab 10.1')
+    ]
+}
+
+# Search Testing Data
+SEARCH_TESTDATA = {
+    'empty_results': [('hello', 0)],
+    'product_results': [('macbook', 3), ('samsung', 1)]
+}
+
+
